9ec7ffe87adab84e850a9729bac2342663f508c1
iSi2aka <n.ishizaka@outlook.com>
2023-08-09 21:10:27 +0900

    シナリオ編集機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_post, only: [:show, :edit, :update ]
 
   def index
     @post = Post.includes(:user).order("created_at DESC")
@@ -19,9 +20,22 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
+  def edit
+    unless current_user.id == @post.user_id 
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @post.update(post_params)
+    if @post.save
+      redirect_to post_path(@post.id)
+    else 
+      render :edit, status: :unprocessable_entity
+    end
+  end
   private
 
   def post_params
@@ -31,5 +45,9 @@ class PostsController < ApplicationController
                   :symptom, :allergy, :medicine, :past_history, :last_meal, :event, :risk_factor, :others, 
                   :story, :point, :image)
           .merge(user_id: current_user.id)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
