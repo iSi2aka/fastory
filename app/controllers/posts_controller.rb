8ae407c98d320493b2635fb66b5c3b4e70e82ef7
iSi2aka <n.ishizaka@outlook.com>
2023-08-09 11:54:53 +0900

    ルーティング、postsコントトローラー定義

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+  def index
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else 
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post)
+          .permit(:title, :disease, :name, :age, :sex_id, :family, 
+                  :airway, :breathing, :circulation, :dysfunction_cns, :exposure,
+                  :symptom, :allergy, :medicine, :past_history, :last_meal, :event, :risk_factor, :others, 
+                  :story, :point)
+          .merge(user_id: current_user.id)
+  end
+end
