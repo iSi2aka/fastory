7a647ae0037432b660fb0e3b68e5d6bc81d36997
iSi2aka <n.ishizaka@outlook.com>
2023-08-09 14:46:55 +0900

    postモデル単体テスト

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,30 @@
 FactoryBot.define do
   factory :post do
-    
+    title           {'test'}
+    disease         {'test'}
+    name            {Faker::Name.initials(number: 2)}
+    age             {Faker::Number.between(from: 0, to: 100)}
+    sex_id          {Faker::Number.between(from: 2, to: 4)}
+    family          {'test'}
+    airway          {'test'}
+    breathing       {'test'}
+    circulation     {'test'}
+    dysfunction_cns {'test'}
+    exposure        {'test'}
+    symptom         {'test'}
+    allergy         {'test'}
+    medicine        {'test'}
+    past_history    {'test'}
+    last_meal       {'test'}
+    event           {'test'}
+    risk_factor     {'test'}
+    others          {'test'}
+    story           {Faker::Lorem.paragraph(sentence_count: 4)}
+    point           {Faker::Lorem.paragraph(sentence_count: 4)}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,141 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @post = FactoryBot.build(:post)
+  end
+
+  describe 'シナリオ投稿機能' do
+
+    context 'シナリオ投稿できる場合' do
+      it 'すべての項目が正しく入力できていれば投稿できる' do
+        expect(@post).to be_valid
+      end 
+      it 'exposureが空でも投稿できる' do
+        @post.exposure = ''
+        expect(@post).to be_valid
+      end
+      it 'othersが空でも投稿できる' do
+        @post.others = ''
+        expect(@post).to be_valid
+      end
+      it 'imageが空でも投稿できる' do
+        @post.image = nil
+        expect(@post).to be_valid
+      end
+    end 
+
+    context 'シナリオ投稿できない場合' do
+      it 'titleが空では投稿できない' do
+        @post.title = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("タイトルを入力してください")
+      end
+      it 'diseaseが空では投稿できない' do
+        @post.disease = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("疾患名を入力してください")
+      end
+      it 'nameが空では投稿できない' do
+        @post.name = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("患者名を入力してください")
+      end
+      it 'ageが空では投稿できない' do
+        @post.age = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("年齢を入力してください")
+      end
+      it "ageが0未満では登録できない" do
+        @post.age = -1
+        @post.valid?
+        expect(@post.errors.full_messages).to include("年齢は0以上の値にしてください")
+      end
+      it "ageが101以上では登録できない" do
+        @post.age = 101
+        @post.valid?
+        expect(@post.errors.full_messages).to include("年齢は100以下の値にしてください")
+      end
+      it "ageが半角数字以外では登録できない" do
+        @post.age = '５０'
+        @post.valid?
+        expect(@post.errors.full_messages).to include("年齢は数値で入力してください")
+      end
+      it 'sex_idが空では投稿できない' do
+        @post.sex_id = 1
+        @post.valid?
+        expect(@post.errors.full_messages).to include("性別を入力してください")
+      end
+      it 'familyが空では投稿できない' do
+        @post.family = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("同乗者/キーパーソンを入力してください")
+      end
+      it 'airwayが空では投稿できない' do
+        @post.airway = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("A 気道を入力してください")
+      end
+      it 'breathingが空では投稿できない' do
+        @post.breathing = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("B 呼吸を入力してください")
+      end
+      it 'circulationが空では投稿できない' do
+        @post.circulation = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("C 循環を入力してください")
+      end
+      it 'dysfunction_cnsが空では投稿できない' do
+        @post.dysfunction_cns = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("D 意識を入力してください")
+      end
+      it 'symptomが空では投稿できない' do
+        @post.symptom = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("主訴を入力してください")
+      end
+      it 'allergyが空では投稿できない' do
+        @post.allergy = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("アレルギーを入力してください")
+      end
+      it 'medicineが空では投稿できない' do
+        @post.medicine = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("服薬歴を入力してください")
+      end
+      it 'past_historyが空では投稿できない' do
+        @post.past_history = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("既往歴を入力してください")
+      end
+      it 'last_mealが空では投稿できない' do
+        @post.last_meal = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("最終飲食を入力してください")
+      end
+      it 'eventが空では投稿できない' do
+        @post.event = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("イベントを入力してください")
+      end
+      it 'risk_factorが空では投稿できない' do
+        @post.risk_factor = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("危険因子を入力してください")
+      end
+      it 'storyが空では投稿できない' do
+        @post.story = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("現病歴・経過を入力してください")
+      end
+      it 'pointが空では投稿できない' do
+        @post.point = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("評価項目を入力してください")
+      end
+    end
+  end
 end
