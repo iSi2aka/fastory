d7f887b0e4ecef3cf8c4fe59c2a2ddd23590a711
iSi2aka <n.ishizaka@outlook.com>
2023-09-01 13:06:49 +0900

    いいね機能修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,12 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_post
 
   def create
     like = current_user.likes.build(post_id: params[:post_id])
     like.save
     respond_to do |format|
-      format.js
+      format.turbo_stream
     end
   end
 
@@ -13,7 +14,7 @@ class LikesController < ApplicationController
     like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     like.destroy
     respond_to do |format|
-      format.js
+      format.turbo_stream
     end
   end
 
