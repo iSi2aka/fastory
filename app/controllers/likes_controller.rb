421ba27515edda6df77175f179c306f359ea8eca
iSi2aka <n.ishizaka@outlook.com>
2023-09-01 14:39:54 +0900

    いいね後現ページにリダイレクト

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,24 +1,16 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_post
 
   def create
     like = current_user.likes.build(post_id: params[:post_id])
     like.save
-    respond_to do |format|
-      format.js
-    end
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     like.destroy
-    respond_to do |format|
-      format.js
-    end
+    redirect_back(fallback_location: root_path)
   end
 
-  def set_post
-    @post = Post.find(params[:post_id])
-  end
 end
