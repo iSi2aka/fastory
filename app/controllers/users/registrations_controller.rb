4934c62cda67d0502b41e8e3de5da3a9898aa68a
iSi2aka <n.ishizaka@outlook.com>
2023-10-01 13:50:26 +0900

    ユーザー情報編集機能 実装

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,18 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  protected
+
+  def update_resource(resource, params)
+    if resource.update_without_password(user_params)
+    end
+  end
+
+  def after_update_path_for(resource)
+    user_path(id: resource.id)
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :department)
+  end
+end
